a53d07ed13d14b018dc026e609814d96ad989fe7
ginjake <hsginjake123@gmail.com>
2023-12-20 02:07:39 +0900

    postで取得できるように

--- src/routes/api.php
+++ src/routes/api.php
@@ -23,4 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::controller(PostController::class)->prefix('board')->group(function () {
     Route::get('post', 'post')->name('post.post');
     Route::get('get', 'get')->name('post.get');
+    Route::post('post', 'post')->name('post.post2');
+    Route::post('get', 'get')->name('post.get2');
 });
